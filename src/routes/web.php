fddbccf60c82119cb009777b6b852f507abd4797
Arvid de Jong <arvid@darvis.nl>
2023-01-05 21:52:25 +0100

    Route update

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use Darvis\Lemmings\App\Http\Livewire\Lemmings;
 use Illuminate\Support\Facades\Route;
 
-
-// Route::get('/lemmings', Lemmings::class)->name('darvis.lemmings');
 Route::get('/lemmings', function () {
-    return view('darvis-lemmings::layouts.website');
+    return view('darvis-lemmings::lemmings');
 });
